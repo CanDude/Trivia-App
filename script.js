072ab2124c378be4f3463b201dd24ae1b9ab802f
Syed Burhan Ali Shah <50847838+CanDude@users.noreply.github.com>
2022-03-29 14:01:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,100 @@
+// get element by ID m # nae lagaty q k wo already ID he pick krta h lk us k illawa koi bhi method use kryngy js ka to us m # laga k define krna hoga ID ko
+const game = document.getElementById('game')
+const scoreDisplay = document.getElementById('score')
+let score = 0
+const genres = [
+    {
+        name:'Comp',
+        id: 11
+    },
+    {
+        name:'Maths',
+        id: 10
+    },
+    {
+        name:'Books',
+        id: 12
+    },
+    {
+        name:'Video Games',
+        id: 15
+    }
+]
+const levels = ['easy', 'medium', 'hard']
+
+function addGenre(genre) {
+    const column = document.createElement('div')
+    
+    column.classList.add('genre-column')
+    column.innerHTML = genre.name
+    // append js ka method h. ye kya kr rha h game k div m is coloumn ko add kr rha h
+    game.append(column)
+
+
+    levels.forEach(level => {
+        const card = document.createElement('div')
+        card.classList.add('card')
+        column.append(card)
+        if(level === 'easy'){
+            card.innerHTML = '100'
+        }
+        if(level === 'medium'){
+            card.innerHTML = '200'
+        }
+        if(level === 'hard'){
+            card.innerHTML = '300'
+        }
+        fetch(`https://opentdb.com/api.php?amount=1&category=${genre.id}&difficulty=${level}&type=boolean`)
+        .then(response => response.json())
+        .then(data => { console.log(data)
+        card.setAttribute('data-question', data.results[0].question)
+        card.setAttribute('data-answer', data.results[0].correct_answer)
+        card.setAttribute('data-value', card.getInnerHTML() )
+        })
+        .then(done =>  card.addEventListener('click', flipCard))
+       
+    })
+}
+genres.forEach(genre => addGenre(genre))
+function flipCard(){
+   this.innerHTML=''
+   this.style.fontSize = '15px'
+    const textDisplay = document.createElement('div')
+    const trueButton = document.createElement('button')
+    const falseButton = document.createElement('button')
+    trueButton.innerHTML = 'True'
+    falseButton.innerHTML = 'False'
+    trueButton.addEventListener('click', getResult)
+    falseButton.addEventListener('click', getResult)
+    textDisplay.innerHTML=this.getAttribute('data-question')
+    this.append(textDisplay, trueButton, falseButton)
+    const allCards = Array.from(document.querySelectorAll('.card'))
+    allCards.forEach(card => card.removeEventListener('click', flipCard))
+}
+
+function getResult(){
+    const allCards = Array.from(document.querySelectorAll('.card'))
+allCards.forEach(card => card.addEventListener('click', flipCard))
+    const cardOfButton = this.parentElement
+    if (cardOfButton.getAttribute('data-answer') === this.innerHTML){
+       score = score + parseInt(cardOfButton.getAttribute('data-value'))
+       scoreDisplay.innerHTML = score
+       cardOfButton.classList.add('correct_answer')
+       setTimeout(()=>{
+           while (cardOfButton.firstChild){
+               cardOfButton.removeChild(cardOfButton.lastChild)
+           }
+           cardOfButton.innerHTML = cardOfButton.getAttribute('data-value')
+       }, 100)
+    } else{
+        cardOfButton.classList.add('wrong_answer')
+        setTimeout(()=>{
+            while (cardOfButton.secondChild){
+                cardOfButton.removeChild(cardOfButton.firstChild)
+            }
+            cardOfButton.innerHTML = 0
+        }, 100)
+
+    }
+cardOfButton.removeEventListener('click', flipCard)
+}
